b21a8fca8a059505686b7c3bd4bb68311534eba7
changxianglin <281532612@qq.com>
2020-11-13 01:59:33 +0800

    button support type

--- src/components/button.vue
+++ src/components/button.vue
@@ -1,15 +1,108 @@
 <template>
-  <button class="lin-button">
+  <button class="lin-button" :class="[`lin-button--${type}`]">
     <span><slot></slot></span>
   </button>
 </template>
 
 <script>
 export default {
-  name: 'LinButton'
+  name: 'LinButton',
+  props: {
+    type: {
+      type: String,
+      default: 'default'
+    }
+  },
+  created () {
+    console.log(this.type)
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.lin-button {
+  display: inline-block;
+  line-height: 1;
+  white-space: nowrap;
+  cursor: pointer;
+  background: #fff;
+  border: 1px solid #dcdfe6;
+  color: #606266;
+  -webkit-appearance: none;
+  text-align: center;
+  box-sizing: border-box;
+  outline: none;
+  margin: 0;
+  transition: .1s;
+  font-weight: 500;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  padding: 12px 20px;
+  font-size: 14px;
+  border-radius: 4px;
+  &:hover,
+  &:focus {
+    color: #409eff;
+    border-color: #c6e2ff;
+    background-color: #ecf5ff;
+  }
+}
+
+.lin-button--primary{
+  color:#fff;
+  background-color: #409eff;
+  border-color: #409eff;
+  &:hover,
+  &:focus{
+    background: #66b1ff;
+    background-color: #66b1ff;
+    color: #fff;
+    }
+  }
+  .lin-button--success{
+  color:#fff;
+  background-color: #67c23a;
+  border-color: #67c23a;
+  &:hover,
+  &:focus{
+    background: #85ce61;
+    background-color: #85ce61;
+    color: #fff;
+    }
+  }
+  .lin-button--info{
+  color:#fff;
+  background-color: #909399;
+  border-color: #909399;
+  &:hover,
+  &:focus{
+    background: #a6a9ad;
+    background-color: #a6a9ad;
+    color: #fff;
+    }
+  }
+  .lin-button--warning{
+  color:#fff;
+  background-color: #e6a23c;
+  border-color: #e6a23c;
+  &:hover,
+  &:focus{
+    background: #ebb563;
+    background-color: #ebb563;
+    color: #fff;
+    }
+  }
+  .lin-button--danger{
+  color:#fff;
+  background-color: #f56c6c;
+  border-color: #f56c6c;
+  &:hover,
+  &:focus{
+    background: #f78989;
+    background-color: #f78989;
+    color: #fff;
+    }
+  }
 
 </style>
